73d45a57ef5e4473387799977b45df1f5cb75325
Catherine <goldyflakes@gmail.com>
2023-09-13 00:25:38 -0400

    got searching working, tv needs some more help

--- app.js
+++ app.js
@@ -4,36 +4,47 @@ dotenv.config();
 const TMDB_KEY = process.env.TMDB_KEY;
 
 window.onload = function() {
+    document.getElementById("media-form").addEventListener("submit", inputForm);
     document.getElementById("fname").addEventListener("input", changeQuery);
 }
 
-function url_vidsrc(isShow, id) {
-    let query = "https://vidsrc.me/embed/" + id;
-    if(isShow) query = "https://vidsrc.me/embed/"+ id + "/" + season + "-" + episode;
-    console.log(query);
-    return query;
+function inputForm(e) {
+    e.preventDefault();
+    clearTimeout(timer);
+    search();
 }
 
-function getPoster(path) {
-    return "https://image.tmdb.org/t/p/w500" + path;
-}
-
-function setDetails(dcontainer, movie, type) {
-    console.log(movie);
+function getPoster(video) {
+    let path = "w154" + video.poster_path;
     
-    dcontainer.addEventListener("click", () => { clickPreview(movie, type) })
+    if(video.backdrop_path)
+        path = "w300" + video.backdrop_path;
+    
+    return "https://image.tmdb.org/t/p/" + path;
+}
+
+function setDetails(dcontainer, video, type) {
+    //console.log(video);
+    
+    dcontainer.addEventListener("click", () => { clickPreview(video, type) })
     
     const title = dcontainer.querySelector(".d-title");
-    const poster = dcontainer.querySelector(".d-poster");
-    const year = dcontainer.querySelector(".d-year");
-    const rating = dcontainer.querySelector(".d-rating");
+    const poster = dcontainer;
+    // const year = dcontainer.querySelector(".d-year");
+    // const rating = dcontainer.querySelector(".d-rating");
     
     dcontainer.style.display = "flex";
-    title.innerText = movie.title;
-    poster.style.backgroundImage = "url('" + getPoster(movie.poster_path) + "')";
+    
+    if(type == "tv")
+        title.innerText = video.name;
+    else
+        title.innerText = video.title;
+    
+    
+    poster.style.backgroundImage = "url('" + getPoster(video) + "')";
    
-    if(movie.Year) year.innerText = movie.release_date;
-    if(movie.imdbRating) rating.innerText = movie.vote_average + " ★";
+    if(video.Year) year.innerText = video.release_date;
+    if(video.imdbRating) rating.innerText = video.vote_average + " ★";
 }
 
 let timer;
@@ -45,10 +56,10 @@ function changeQuery() {
 
 async function search() {
     const query = document.getElementById("fname").value;
-    console.log(query)
+    //console.log(query)
     
     getResults(query, "movie");
-    //getResults(query, "TV");
+    getResults(query, "tv");
 }
 
 async function getResults(query, type) {
@@ -99,7 +110,9 @@ async function queryTMDB(query, type) {
 }
 
 async function detailsTMDB(id, type) {
-    const url = 'https://api.themoviedb.org/3/' + type + '/' + id + '?language=en-US';
+    let extra = "?language=en-US";
+    if(type == "tv") extra = "/external_ids"
+    const url = 'https://api.themoviedb.org/3/' + type + '/' + id + extra;
     const options = {
         method: 'GET',
         headers: {
@@ -122,10 +135,12 @@ async function clickPreview(preview, type) {
     showDiscover(false);
     const details = await detailsTMDB(preview.id, type);
     
-    if(type == "TV") {
+    if(type == "tv") {
         //have to get season & episode info first
         showTVInfo(true);
         // SET SEASON & EPISODE
+        const url = await getVideo(details.imdb_id, type);
+        showVideo(url);
     }
     else {
         //show movie right away
@@ -136,7 +151,7 @@ async function clickPreview(preview, type) {
 
 function getVideo(id, type) {    
     //try vidsrc
-    let url = url_vidsrc(type == "TV" ? true : false, id);
+    let url = url_vidsrc(type == "tv" ? true : false, id);
     if(!url) return null;
     
     
@@ -148,14 +163,24 @@ function showVideo(url) {
     document.getElementById("embedsrc").src = url;
 }
 
+function url_vidsrc(isShow, id) {
+    const season = document.getElementById('fseason').value;
+    const episode = document.getElementById('fepisode').value;
+    
+    let query = "https://vidsrc.me/embed/" + id;
+    if(isShow) query = "https://vidsrc.me/embed/"+ id + "/" + season + "-" + episode;
+    console.log(query);
+    return query;
+}
+
 
 function showDiscover(show) {
     if(!show) { //hide
         clearTimeout(timer);
-        document.getElementById("movie-results").style.display = "none";
+        document.getElementById("search-results").style.display = "none";
     }
     else { //show
-        document.getElementById("movie-results").style.display = "flex";
+        document.getElementById("search-results").style.display = "flex";
     }
 }
 
