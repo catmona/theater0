34a9e4c730d2a707fa00c8cb8c4efa70d88d756f
Catherine Sangiovanni <catherine.sangiovanni@gmail.com>
2023-09-14 16:10:52 -0400

    converting to vite + react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Switch } from '@headlessui/react'
+
+function App() {
+    return (
+        <>
+            <h1 className="font-semibold">test</h1>
+            <Switch className="inline-block h-6 w-11 bg-black transition" />
+        </>
+    )
+}
+
+export default App
